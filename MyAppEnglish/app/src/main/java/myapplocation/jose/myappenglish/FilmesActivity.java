032fa5e133c303acb51372d4cfffcac87fdfa757
kairiroberto <kairiroberto@gmail.com>
2018-05-17 11:33:03 -0300

    Criação do projeto

---
+++ MyAppEnglish/app/src/main/java/myapplocation/jose/myappenglish/FilmesActivity.java
@@ -0,0 +1,13 @@
+package myapplocation.jose.myappenglish;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FilmesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filmes);
+    }
+}
